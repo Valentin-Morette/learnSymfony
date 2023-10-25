62b1dc2966d06c56954e5c7f5042f2a721797d39
valentin-morette <morette.valentin@gmail.com>
2023-10-25 19:12:03 +0200

    finish get and start post

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class IngredientController extends AbstractController
 {
-	#[Route('/ingredient', name: 'app_ingredient')]
+	#[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
 	public function index(
 		IngredientRepository $repository,
 		Paginator $paginator,
@@ -20,11 +20,17 @@ class IngredientController extends AbstractController
 		$ingredients = $paginator->paginate(
 			$repository->findAll(),
 			$request->query->getInt('page', 1),
-			10
+			15
 		);
 
 		return $this->render('pages/ingredient/index.html.twig', [
 			'ingredients' => $ingredients,
 		]);
+	}
+
+	#[Route('/ingredient/new', name: 'ingredient_new', methods: ['GET', 'POST'])]
+	public function new(): Response
+	{
+		return $this->render('pages/ingredient/new.html.twig');
 	}
 }
